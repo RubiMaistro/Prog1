fe4b4d32b5c66ac73371cb463b7b3869f7c6b4a9
RubiMaistro <55796855+RubiMaistro@users.noreply.github.com>
2020-03-04 10:34:20 +0100

    Burn-tétel

---
+++ burn.R
@@ -0,0 +1,15 @@
+library(matlab)
+stp <- function(x){
+primes = primes(x) 
+diff = primes[2:length(primes)]-primes[1:length(primes)-1] 
+idx = which(diff==2) 
+t1primes = primes[idx] 
+t2primes = primes[idx]+2 rt1plust2 = 1/t1primes+1/t2primes 
+return(sum(rt1plust2))
+}
+x=seq(13, 1000000, by=10000) 
+y=sapply(x, FUN = stp) 
+plot(x,y,type="b")
+
+&& Megoldás forrása: 
+&& https://gitlab.com/nbatfai/bhax/blob/master/attention_raising/Primek_R
